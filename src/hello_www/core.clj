b0758df4f3cc32138f76f2fe7ac5bc0f64bad714
calebphillips <caleb.phillips@gmail.com>
2011-03-26 23:16:17 -0500

    Added shell of adding a new cd (doesn't add to the db yet)

--- src/hello_www/core.clj
+++ src/hello_www/core.clj
@@ -2,6 +2,7 @@
     (:use compojure.core)
     (:use hiccup.core)
     (:use hiccup.page-helpers)
+    (:use ring.util.response)
     (:require [compojure.route :as route]
               [compojure.handler :as handler]))
 
@@ -15,23 +16,51 @@
                    (struct cd "I and Love and You" "The Avett Brothers")
                    (struct cd "The Medicine" "John Mark McMillan")))
 
-(defn db-to-html [db]
-      (map (fn [cd] [:tr 
-                      [:td (:title cd)]
-                      [:td (:artist cd)]]) db))
+(defn cd-rows [db]
+      (map (fn [{:keys [title artist]}] 
+               [:tr 
+                 [:td title]
+                 [:td artist]]) db))
 
-(defn home-page []
+(defn view-layout [& content]
       (html
         (doctype :xhtml-strict)
         (xhtml-tag "en"
-                   [:body [:h1 "The Home Page"]  
-                          [:table {:border 1}
-                            [:tr
-                              [:th "Title"] [:th "Artist"]]
-                            (db-to-html (init-db))]])))
+                   [:head
+                     [:meta {:http-equiv "Content-type"
+                            :content "text/html; charset=utf-8"}]
+                     [:title "Play List"]]
+                   [:body content])))
+
+(defn home-page []
+      (view-layout
+        [:h1 "The Play List"]  
+        [:table {:border 1}
+                [:tr
+                  [:th "Title"] [:th "Artist"]]
+                (cd-rows (init-db))]
+        [:p [:a {:href "/cds/new"} "New CD"]]
+        ))
+
+(defn new-cd-view []
+      (view-layout
+        [:h1 "Add a New CD"]
+        [:form {:method "post" :action "/cds"}
+               [:input {:type "text" :name "title"}]
+               [:br]
+               [:input {:type "text" :name "artist"}]
+               [:br]
+               [:input {:type "submit" :value "add"}]]))
+
+(defn add-cd [cd]
+      (println "adding cd"))
+
 (defroutes main-routes
-             (GET "/" [] (home-page))
-             (GET "/user/:id" [id] (str "I sense that you are user " id))
+             (GET "/cds" [] (home-page))
+             (GET "/cds/new" [] (new-cd-view))
+             (POST "/cds" [title artist] 
+                   (add-cd (struct cd title artist))
+                   (redirect "/cds"))
              (route/resources "/")
              (route/not-found "Page not found"))
 
